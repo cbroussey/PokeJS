dfb84f9c38b141bd589fb5039abe39353b97f40e
Colin Broussey <colin.broussey@etudiant.univ-rennes1.fr>
2024-03-20 19:25:58 +0100

    commentaires 99%

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
-function setFn(caller) {
-    let cmd = caller.innerText.match(/^[^ \(]+/)[0]
-    let args = caller.innerText.match(/\([^\)]*/)[0].substring(1).split(",")
+function setFn(caller) { // Load a function and its arguments in the form
+    let cmd = caller.innerText.match(/^[^ \(]+/)[0] // Get the function name
+    let args = caller.innerText.match(/\([^\)]*/)[0].substring(1).split(",") // Get the arguments
     $("#function").val(cmd)
     showArgs()
     if (args[0] != "") {
-        args.forEach((e,i) => {
+        args.forEach((e,i) => { // Fill the arguments
             document.getElementById("params").children[i].value = e.substring(1, e.length-1)
         })
     }
 }
 
-function runFn() {
-    let cmd = $("#function > option:selected").val()
-    let args = [...$("#params > input").map((i,el) => el.value)]
+function runFn() { // Run the selected function with the entered arguments
+    let cmd = $("#function > option:selected").val() // Get the selected function name
+    let args = [...$("#params > input").map((i,el) => el.value)] // Get the arguments
     args.forEach((el,i) => {
-        if (!isNaN(el)) args[i] = parseInt(el)
-        else args[i] = `'${el}'`
+        if (!isNaN(el)) args[i] = parseInt(el) // Convert to int if possible
+        else args[i] = `'${el}'` // Else add quotes
     })
     $("#result")[0].innerHTML = ""
     try {
-        let res = eval(`${cmd}(${args.join(",")})`)
+        let res = eval(`${cmd}(${args.join(",")})`) // Run the function
         if (res == "") console.log("No results") //$("#result")[0].innerHTML = "No results"
         else {
             console.table(res)
@@ -32,16 +32,16 @@ function runFn() {
     }
 }
 
-function showArgs() {
+function showArgs() { // Add input fields for every argument of the selected function
     let cmd = $("#function > option:selected").val()
-    let args = eval(`${cmd}.toString()`).match(/^function [^ ]+( )*\([^\)]*\)/gm)[0]
-    args = args.substring(args.indexOf("(")+1,args.length-1).split(",")
+    let args = eval(`${cmd}.toString()`).match(/^function [^ ]+( )*\([^\)]*\)/gm)[0] // Get the parameters names of the function
+    args = args.substring(args.indexOf("(")+1,args.length-1).split(",") // Remove the function name and parenthesis
     $("#params")[0].innerHTML = ""
     for (let i of args) {
         if (i != "") $("#params")[0].innerHTML += `<input type="text" placeholder="${i}">`
     }
     document.querySelectorAll("#params > input").forEach(e => {
-        e.addEventListener('keyup', (e) => {
+        e.addEventListener('keyup', (e) => { // Run the function when pressing Enter (alternative to clicking the "Run" button)
             if (e.key === 'Enter' || e.keyCode === 13) {
                 runFn()
             }
@@ -72,8 +72,8 @@ function sortPokemonByStamina() {
 function getWeakestEnnemies(attack) {
     let attackType = new Attack(attack).type
     let weakest = []
-    Object.values(Pokemon.all_pokemons).forEach((e, f) => {
-        weakest.push({id : f, multiplier : Type.effectivenessCalc(attackType, e["types"])})
+    Object.values(Pokemon.all_pokemons).forEach(e => { // Calculate the effectiveness of the attack on every pokemon
+        weakest.push({id : e["id"], multiplier : Type.effectivenessCalc(attackType, e["types"])})
     })
     // The bigger the multiplier, the stronger the attack, the weaker the ennemy
     weakest = weakest.sort((a, b) => a["multiplier"] > b["multiplier"] ? -1 : 1)
@@ -84,8 +84,8 @@ function getWeakestEnnemies(attack) {
 function getStrongestEnnemies(attack) {
     let attackType = new Attack(attack).type
     let strongest = []
-    Object.values(Pokemon.all_pokemons).forEach((e, f) => {
-        strongest.push({id : f, multiplier : Type.effectivenessCalc(attackType, e["types"])})
+    Object.values(Pokemon.all_pokemons).forEach(e => { // Calculate the effectiveness of the attack on every pokemon
+        strongest.push({id : e["id"], multiplier : Type.effectivenessCalc(attackType, e["types"])})
     })
     // The lower the multiplier, the weaker the attack, the stronger the ennemy
     strongest = strongest.sort((a, b) => a["multiplier"] < b["multiplier"] ? -1 : 1)
