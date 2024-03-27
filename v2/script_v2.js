6f23b5eecd4cdecda42141e880f4d75087ad649b
Colin Broussey <colin.broussey@etudiant.univ-rennes1.fr>
2024-03-27 15:29:50 +0100

    avancement pagination

--- v2/script_v2.js
+++ v2/script_v2.js
@@ -1,7 +1,8 @@
+const dispAmount = 25
+
 function displayList(page = 0) {
-    const dispAmnt = 25
     let pkmns = Object.values(Pokemon.all_pokemons)
-    for (let n = page * dispAmnt; n < pkmns.length && n < (page+1) * dispAmnt; n++) {
+    for (let n = page * dispAmount; n < pkmns.length && n < (page+1) * dispAmount; n++) {
         let pkrow = document.createElement("tr")
         Object.values(document.querySelectorAll("#pokeList > thead > tr > th")).forEach(i => {
             let pkcell = document.createElement("td")
@@ -17,9 +18,26 @@ function displayList(page = 0) {
         })
         document.querySelector("#pokeList > tbody").appendChild(pkrow)
     }
+    $("#pageNum").text("Page " + (page + 1) + "/" + Math.ceil(pkmns.length / dispAmount))
 }
 
 $(document).ready(() => {
     Pokemon.import_pokemons();
     displayList();
+    $("#prev").click(() => {
+        let page = parseInt($("#pageNum").text().split(" ")[1].split("/")[0]) - 1
+        if (page > 0) {
+            $("#pokeList > tbody").empty()
+            displayList(page - 1)
+            document.body.scrollIntoView()
+        }
+    })
+    $("#next").click(() => {
+        let page = parseInt($("#pageNum").text().split(" ")[1].split("/")[0]) - 1
+        if (page < Math.floor(Object.values(Pokemon.all_pokemons).length / dispAmount)) {
+            $("#pokeList > tbody").empty()
+            displayList(page + 1)
+            document.body.scrollIntoView()
+        }
+    })
 })
