98a36535c6d5ea0de4aafb63499ec6a70db5dc34
cbroussey <colin.broussey@etudiant.univ-rennes1.fr>
2024-04-14 21:49:08 +0200

    infos finies

--- v3/script_v3.js
+++ v3/script_v3.js
@@ -1,4 +1,72 @@
 const dispAmount = 25
+const typeColors = {
+    "Normal": '#A8A77A',
+	"Fire": '#EE8130',
+	"Water": '#6390F0',
+	"Electric": '#F7D02C',
+	"Grass": '#7AC74C',
+	"Ice": '#96D9D6',
+	"Fighting": '#C22E28',
+	"Poison": '#A33EA1',
+	"Ground": '#E2BF65',
+	"Flying": '#A98FF3',
+	"Psychic": '#F95587',
+	"Bug": '#A6B91A',
+	"Rock": '#B6A136',
+	"Ghost": '#735797',
+	"Dragon": '#6F35FC',
+	"Dark": '#705746',
+	"Steel": '#B7B7CE',
+	"Fairy": '#D685AD',
+}
+
+function detailsPopup(e) {
+    //console.log(e.currentTarget.children[0].innerText)
+    $("body").css("overflow", "hidden") // Prevent scrolling
+    $("#popup").show(0, () => {
+        $("#popup").css("opacity", ".5")
+    })
+    $("#popup").click(() => {
+        document.getElementById("popup").addEventListener("transitionend", f => {
+            $("#popup").hide()
+            $("body").css("overflow", "auto") // Restore scrolling
+        }, {once: true})
+        document.getElementById("detailsPopup").addEventListener("transitionend", f => {
+            $("#detailsPopup").hide()
+        }, {once: true})
+        $("#popup").css("opacity", "0")
+        $("#detailsPopup").css("transform", "")
+        $("#detailsPopup").css("opacity", "0")
+    })
+    let pkmn = new Pokemon(e.currentTarget.children[0].innerText)
+    $("#detailsImg").attr("src", "../webp/images/" + (pkmn.id < 100 ? "0" : "") + (pkmn.id < 10 ? "0" : "") + pkmn.id + ".webp")
+    $("#detailsName").text(pkmn.name)
+    $("#detailsForm").text(pkmn.form)
+    $("#detailsGen").text("(Gen " + pkmn.generation + ")")
+    $("#detailsTypes")[0].innerHTML = "<h3>Types: </h3>"
+    pkmn.types.forEach(f => {
+        $("#detailsTypes")[0].innerHTML += "<p style='background-color: " + typeColors[f] + "'>" + f + "</p>"
+    })
+    $("#detailsAtt").text(pkmn.base_attack)
+    $("#detailsAtt")[0].innerHTML += "<div class='tooltiptext'>Attack</div>"
+    $("#detailsDef").text(pkmn.base_defense)
+    $("#detailsDef")[0].innerHTML += "<div class='tooltiptext'>Defense</div>"
+    $("#detailsSta").text(pkmn.base_stamina)
+    $("#detailsSta")[0].innerHTML += "<div class='tooltiptext'>Stamina</div>"
+    $("#detailsFast > ul").empty()
+    pkmn.fast_moves.forEach(f => {
+        $("#detailsFast > ul")[0].innerHTML += "<li>" + f + "</li>"
+    })
+    $("#detailsCharged > ul").empty()
+    pkmn.charged_moves.forEach(f => {
+        $("#detailsCharged > ul")[0].innerHTML += "<li>" + f + "</li>"
+    })
+    $("#detailsPopup").show(0, () => {
+        $("#detailsPopup").css("display", "flex")
+        $("#detailsPopup").css("opacity", "1")
+        $("#detailsPopup").css("transform", "translateY(0)")
+    })
+}
 
 function displayList(page = 0) {
     $("#prev").prop("disabled", false)
@@ -20,6 +88,9 @@ function displayList(page = 0) {
         })
         document.querySelector("#pokeList > tbody").appendChild(pkrow)
     }
+    $("tbody >  tr").click(e => {
+        detailsPopup(e)
+    })
     $("#pageNum").text("Page " + (page + 1) + "/" + Math.ceil(pkmns.length / dispAmount))
     if (page == 0) $("#prev").prop("disabled", true)
     if (page == Math.ceil(pkmns.length / dispAmount)-1) $("#next").prop("disabled", true)
@@ -36,8 +107,6 @@ $(document).ready(() => {
             $("#pokeList > tbody").empty()
             displayList(page - 1)
             document.body.scrollIntoView()
-        } else {
-            
         }
     })
     $("#next").click(() => {
@@ -48,30 +117,5 @@ $(document).ready(() => {
             document.body.scrollIntoView()
         }
     })
-    $("tbody >  tr").click(e => {
-        console.log(e.currentTarget.children[0].innerText)
-        $("body").css("overflow", "hidden") // Prevent scrolling
-        $("#popup").show(0, () => {
-            $("#popup").css("opacity", ".5")
-        })
-        $("#popup").click(() => {
-            document.getElementById("popup").addEventListener("transitionend", f => {
-                $("#popup").hide()
-                $("body").css("overflow", "auto") // Restore scrolling
-            }, {once: true})
-            document.getElementById("detailsPopup").addEventListener("transitionend", f => {
-                $("#detailsPopup").hide()
-            }, {once: true})
-            $("#popup").css("opacity", "0")
-            $("#detailsPopup").css("transform", "")
-            $("#detailsPopup").css("opacity", "0")
-        })
-        let pkmn = new Pokemon(e.currentTarget.children[0].innerText)
-        $("#detailsPopup > img").attr("src", "../webp/images/" + (pkmn.id < 100 ? "0" : "") + (pkmn.id < 10 ? "0" : "") + pkmn.id + ".webp")
-        $("#detailsPopup").show(0, () => {
-            $("#detailsPopup").css("display", "flex")
-            $("#detailsPopup").css("opacity", "1")
-            $("#detailsPopup").css("transform", "translateY(0)")
-        })
-    })
+    
 })
